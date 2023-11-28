5be52b40326041964a337b76917d0bd3c9f554b8
tomcoso <tomasdessy0@gmail.com>
2023-11-28 19:16:12 +1300

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+
+const debug = require("debug")("app");
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+// MONGODB connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI || process.env.MONGODB_DEV_URI;
+
+main().catch((err) => debug(`Cannot connect to database: ${err}`));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const RateLimiter = require("express-rate-limiter");
+const limiter = RateLimiter({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// middleware chain
+app.use(limiter);
+app.use(compression());
+app.use(helmet());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;

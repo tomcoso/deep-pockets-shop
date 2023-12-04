779fec763b7fd22e4ae7d694594c3b3fb6136923
tomcoso <tomasdessy0@gmail.com>
2023-12-05 10:51:26 +1300

    add update category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Product = require("../models/product");
 
 const debug = require("debug")("deep-pockets-shop:category");
 
@@ -68,8 +69,59 @@ exports.category_create_post = [
   }),
 ];
 
-exports.category_update_get = asyncHandler(async (req, res, next) => {});
-exports.category_update_post = asyncHandler(async (req, res, next) => {});
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  const category = await Category.findById(req.params.id).exec();
+
+  if (!category) {
+    res.redirect("/inventory/category/all");
+    return;
+  }
+
+  res.render("inventory/category_form", {
+    title: "Update category",
+    category: category,
+    errors: undefined,
+  });
+});
+exports.category_update_post = [
+  body("name")
+    .trim()
+    .isLength({ min: 3 })
+    .isAlpha()
+    .withMessage("Name cannot contain numbers")
+    .escape(),
+  body("description")
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage("Description must be at least 10 characters.")
+    .isAlphanumeric()
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const updatedCategory = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("inventory/category_form", {
+        title: "Update category",
+        category: updatedCategory,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await Category.findByIdAndUpdate(
+        updatedCategory._id,
+        updatedCategory,
+        {}
+      );
+      res.redirect(updatedCategory.url);
+    }
+  }),
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {});
 exports.category_delete_post = asyncHandler(async (req, res, next) => {});

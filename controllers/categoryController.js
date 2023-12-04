bbdf6929ed9ea749b016ae9f01602b302800d372
tomcoso <tomasdessy0@gmail.com>
2023-12-04 15:33:32 +1300

    add category create

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,9 @@
 const Category = require("../models/category");
 
+const debug = require("debug")("deep-pockets-shop:category");
+
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
   const allCategories = await Category.find({}).exec();
@@ -20,8 +23,50 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.category_create_get = asyncHandler(async (req, res, next) => {});
-exports.category_create_post = asyncHandler(async (req, res, next) => {});
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.render("inventory/category_form", {
+    title: "Create category",
+    errors: undefined,
+    category: undefined,
+  });
+});
+exports.category_create_post = [
+  body("name")
+    .trim()
+    .isLength({ min: 3 })
+    .isAlpha()
+    .withMessage("Name cannot contain numbers")
+    .escape(),
+  body("description")
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage("Description must be at least 10 characters.")
+    .isAlphanumeric()
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      debug(category);
+
+      res.render("inventory/category_form", {
+        title: "Create category",
+        errors: errors.array(),
+        category: category,
+      });
+      return;
+    } else {
+      await category.save();
+
+      res.redirect(category.url);
+    }
+  }),
+];
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {});
 exports.category_update_post = asyncHandler(async (req, res, next) => {});
--- routes/inventory.js
+++ routes/inventory.js
@@ -9,8 +9,6 @@ router.get("/", product_controller.inventory_index);
 // PRODUCTS --------------------------------------------------------
 router.get("/products/all", product_controller.product_list_all);
 
-router.get("/products/:category", product_controller.product_list_category);
-
 // products CRUD
 router.get("/products/detail/:id", product_controller.product_detail);
 
@@ -19,29 +17,32 @@ router.get("/products/create", product_controller.product_create_get);
 router.post("/products/create", product_controller.product_create_post);
 
 // update
-router.get("/products/update", product_controller.product_update_get);
-router.post("/products/update", product_controller.product_update_post);
+router.get("/products/:id/update", product_controller.product_update_get);
+router.post("/products/:id/update", product_controller.product_update_post);
 
 // delete
-router.get("/products/delete", product_controller.product_delete_get);
-router.post("/products/delete", product_controller.product_delete_post);
+router.get("/products/:id/delete", product_controller.product_delete_get);
+router.post("/products/:id/delete", product_controller.product_delete_post);
+
+router.get("/products/:category", product_controller.product_list_category);
 
 // CATEGORIES ------------------------------------------------------
 router.get("/category/all", category_controller.category_list);
 
 // categories CRUD
-router.get("/category/:id", category_controller.category_detail);
 
 // create
 router.get("/category/create", category_controller.category_create_get);
 router.post("/category/create", category_controller.category_create_post);
 
 // update
-router.get("/category/update", category_controller.category_update_get);
-router.post("/category/update", category_controller.category_update_post);
+router.get("/category/:id/update", category_controller.category_update_get);
+router.post("/category/:id/update", category_controller.category_update_post);
 
 // delete
-router.get("/category/delete", category_controller.category_delete_get);
-router.post("/category/delete", category_controller.category_delete_post);
+router.get("/category/:id/delete", category_controller.category_delete_get);
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+router.get("/category/:id", category_controller.category_detail);
 
 module.exports = router;
